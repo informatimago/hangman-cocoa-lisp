41d357126a5e36720fcd84c6c08b2aef7dbcc9cd
Pascal J. Bourguignon <pjb@informatimago.com>
2018-08-06 20:47:51 +0200

    Deleted trailing whitespaces.

--- src/hangman-controller.lisp
+++ src/hangman-controller.lisp
@@ -5,9 +5,9 @@
 ;;;;SYSTEM:             Common-Lisp
 ;;;;USER-INTERFACE:     NONE
 ;;;;DESCRIPTION
-;;;;    
+;;;;
 ;;;;    Hangman Controller.
-;;;;    
+;;;;
 ;;;;AUTHORS
 ;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
 ;;;;MODIFICATIONS
@@ -15,19 +15,19 @@
 ;;;;BUGS
 ;;;;LEGAL
 ;;;;    AGPL3
-;;;;    
+;;;;
 ;;;;    Copyright Pascal J. Bourguignon 2015 - 2015
-;;;;    
+;;;;
 ;;;;    This program is free software: you can redistribute it and/or modify
 ;;;;    it under the terms of the GNU Affero General Public License as published by
 ;;;;    the Free Software Foundation, either version 3 of the License, or
 ;;;;    (at your option) any later version.
-;;;;    
+;;;;
 ;;;;    This program is distributed in the hope that it will be useful,
 ;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;;;    GNU Affero General Public License for more details.
-;;;;    
+;;;;
 ;;;;    You should have received a copy of the GNU Affero General Public License
 ;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;;;**************************************************************************
@@ -207,7 +207,7 @@
 ;; application delegate methods:
 
 @[HangmanController
-  method: (applicationDidFinishLaunching:(:id)aNotification) 
+  method: (applicationDidFinishLaunching:(:id)aNotification)
   resultType: (:void)
   body:
   (declare (ignore aNotification))
@@ -222,14 +222,14 @@
   (initialize-game self)]
 
 @[HangmanController
-  method: (applicationWillTerminate:(:id)aNotification) 
+  method: (applicationWillTerminate:(:id)aNotification)
   resultType: (:void)
   body:
   (declare (ignore aNotification))
   (finalize-game self)]
 
 @[HangmanController
-  method: (applicationShouldTerminate:(:id)sender) 
+  method: (applicationShouldTerminate:(:id)sender)
   resultType: (:int)
   body:
   (declare (ignore sender))
@@ -396,5 +396,3 @@
 
 
 ;;;; THE END ;;;;
-
-
